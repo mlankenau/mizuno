ca5dde1992f10a079e57e82583f3813f2953fbcb
Rudolf Schmidt <me@rudionrails.com>
2011-09-15 18:12:28 +0200

    updated rake tasks

--- Rakefile
+++ Rakefile
@@ -1,7 +1,10 @@
 $: << 'lib'
 
 require 'mizuno/rake_tasks'
-Mizuno::RakeTasks.new
+Mizuno::RakeTasks.new do |m|
+  m.pidfile = "mizuno.pid"
+  m.outfile = "mizuno.out"
+end
 
 require 'rspec/core/rake_task'
 RSpec::Core::RakeTask.new(:spec)
--- lib/mizuno/rake_tasks.rb
+++ lib/mizuno/rake_tasks.rb
@@ -5,7 +5,6 @@ module Mizuno
   class RakeTasks < ::Rake::TaskLib
     include ::Rake::DSL if defined?(::Rake::DSL)
 
-    
     attr_accessor :env
     attr_accessor :cmd, :port
     attr_accessor :pidfile, :outfile
@@ -17,49 +16,52 @@ module Mizuno
 
       yield self if block_given?
 
-      define_tasks!
-    end
-
-    
-    private
-
-    def define_tasks!
       namespace :mizuno do
         desc "Start Mizuno"
-        task :start do
-          if running?
-            warn "* An instance of mizuno is already running"
-            Rake::Task[ "mizuno:stop" ].invoke
-          end
-
-          puts "Starting mizuno in #{env}"
-          command = "#{cmd} -p #{port} -E #{env} -P #{pidfile} >> #{outfile} 2>&1 &"
-          sh( command ) { |ok, status| ok or fail "failed with status (#{status.exitstatus})" }
-        end
+        task(:start) { start }
 
         desc "Stop Mizuno"
-        task :stop do
-          if running?
-            puts "Stopping mizuno (pid: #{pid})"
-            kill( pid )
-          else
-            puts "Mizuno not running"
-          end
-        end
+        task(:stop) { stop }
 
         desc "Restart Mizuno"
         task :restart => [:stop, :start]
 
         desc "Status Mizuno"
-        task :status do
-          if running?
-            puts "Mizuno: running (pid: #{pid})"
-          else
-            puts "Mizuno: not running"
-          end
-        end
+        task(:status) { status }
       end
     end
+
+    
+    private
+
+    def start
+      if running?
+        warn "* An instance of mizuno is already running"
+        stop
+      end
+
+      puts "Starting mizuno in #{env}"
+      command = "#{cmd} -p #{port} -E #{env} -P #{pidfile} >> #{outfile} 2>&1 &"
+      sh( command ) { |ok, status| ok or fail "failed with status (#{status.exitstatus})" }
+    end
+
+    def stop
+      if running?
+        puts "Stopping mizuno (pid: #{pid})"
+        kill( pid )
+      else
+        puts "Mizuno not running"
+      end
+    end
+
+    def status
+      if running?
+        puts "Mizuno: running (pid: #{pid})"
+      else
+        puts "Mizuno: not running"
+      end
+    end
+
 
     def running?
       return false unless File.exist?( pidfile )
