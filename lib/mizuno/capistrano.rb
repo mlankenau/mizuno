81d2bfa871caf4038b50450a742fff09457f86a7
Rudolf Schmidt <me@rudionrails.com>
2011-09-16 11:13:25 +0200

    added RACK_ENV to restart task

--- lib/mizuno/capistrano.rb
+++ lib/mizuno/capistrano.rb
@@ -6,18 +6,17 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :mizuno do
     desc "Start mizuno"
     task :start, :roles => :app, :except => { :no_release => true } do
-      # makes use of capistrano/ext/multistage
-      run "cd #{current_path} && RACK_ENV=#{fetch(:stage, 'staging')} bundle exec rake mizuno:start"
+      run "cd #{current_path} && RACK_ENV=#{fetch(:rack_env, 'staging')} bundle exec rake mizuno:start"
     end
 
     desc "Stop mizuno"
     task :stop, :roles => :app, :except => { :no_release => true }, :on_error => :continue do
-      run "cd #{current_path} && bundle exec rake izuno:stop"
+      run "cd #{current_path} && bundle exec rake mizuno:stop"
     end
 
     desc "Restart mizuno"
     task :restart, :roles => :app, :except => { :no_release => true } do
-      run "cd #{current_path} && bundle exec rake mizuno:restart"
+      run "cd #{current_path} && RACK_ENV=#{fetch(:rack_env, 'staging')} bundle exec rake mizuno:restart"
     end
   end
   
