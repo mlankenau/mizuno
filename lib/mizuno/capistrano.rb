bb1df22b7fde6a0572bd78fc59da8985fa46d8b0
Rudolf Schmidt <me@rudionrails.com>
2011-09-30 13:59:36 +0200

    added pipefail to nohup to top execution upon errors

--- lib/mizuno/capistrano.rb
+++ lib/mizuno/capistrano.rb
@@ -6,17 +6,21 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :mizuno do
     desc "Start mizuno"
     task :start, :roles => :app, :except => { :no_release => true } do
-      run "cd #{current_path} && RACK_ENV=#{fetch(:rack_env, 'staging')} nohup bundle exec rake mizuno:start | tee"
+      run_with_pipefail "cd #{current_path} && RACK_ENV=#{fetch(:rack_env, 'staging')} nohup bundle exec rake mizuno:start | tee"
     end
 
     desc "Stop mizuno"
     task :stop, :roles => :app, :except => { :no_release => true }, :on_error => :continue do
-      run "cd #{current_path} && nohup bundle exec rake mizuno:stop | tee"
+      run_with_pipefail "cd #{current_path} && nohup bundle exec rake mizuno:stop | tee"
     end
 
     desc "Restart mizuno"
     task :restart, :roles => :app, :except => { :no_release => true } do
-      run "cd #{current_path} && RACK_ENV=#{fetch(:rack_env, 'staging')} nohup bundle exec rake mizuno:restart | tee"
+      run_with_pipefail "cd #{current_path} && RACK_ENV=#{fetch(:rack_env, 'staging')} nohup bundle exec rake mizuno:restart | tee"
+    end
+
+    def run_with_pipefail( cmd )
+      run "set -o pipefail; " << cmd
     end
   end
   
